c6cbd9c520c9352544aee0939c06a2301521f0cb
Rex Kho <rexkho510@gmail.com>
2023-01-12 14:02:12 -0800

    Added a Logo

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,42 +1,44 @@
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import { NavLink } from "react-router-dom";
+import logo from "../../Assets/Images/helpLogo.png"
 import './Navigation.css';
 
 
-function Navigation(){
+function Navigation() {
     const sessionUser = useSelector(state => state.session.user);
     let sessionLinks;
-    if(sessionUser) {
+    if (sessionUser) {
         sessionLinks = (
             <ProfileButton user={sessionUser} />
         );
     } else {
         sessionLinks = (
             <>
-                
-                    <NavLink to="/login" id ="seslinks" className="navbutton">
-                        <i className="fa-sharp fa-solid fa-right-to-bracket"></i>
-                    </NavLink>
-                    
-               
-                    <NavLink to="/signup" id ="seslinks" className="navbutton">
-                        <i className="fa-sharp fa-solid fa-user-plus"></i>
-                    </NavLink>
-                
+
+                <NavLink to="/login" id="seslinks" className="navbutton">
+                    <i className="fa-sharp fa-solid fa-right-to-bracket"></i>
+                </NavLink>
+
+
+                <NavLink to="/signup" id="seslinks" className="navbutton">
+                    <i className="fa-sharp fa-solid fa-user-plus"></i>
+                </NavLink>
+
             </>
         );
     }
 
 
-    
+
 
     return (
         <>
-            <ul id ="navbar">
-                    <NavLink exact to="/" className="navbutton">
-                        <i className="fa-sharp fa-solid fa-house"></i>
-                    </NavLink>
+            <ul id="navbar">
+                <NavLink exact to="/" className="navbutton">
+                    <img src={logo} alt="logo" id="logo"></img>
+                    {/* <i className="fa-sharp fa-solid fa-house"></i> */}
+                </NavLink>
                 <div>
                     {sessionLinks}
                 </div>
