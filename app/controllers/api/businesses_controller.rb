422fe990066ad4b6a0f1624c586699f5114b68e2
Rex Kho <rexkho510@gmail.com>
2023-02-01 14:28:48 -0800

    Business Index Controller Compatible with Search

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -1,6 +1,20 @@
 class Api::BusinessesController < ApplicationController
     def index 
-        @businesses = Business.all 
+        # @businesses = Business.all 
+
+        search = params[:search].downcase.split(" ") if params[:search]
+
+        @filtered_businesses = []
+
+        if params[:search]
+            search.each do |term|
+                @filtered_businesses << Business.where("LOWER(name) LIKE ?", "%#{term}%")
+            end
+        else
+            @filtered_businesses = Business.all
+        end
+
+        @business = @filtered_businesses
         render :index
     end
 
